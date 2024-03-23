0e3e00d18183f1403b675949d0327b869a10d8fa
KUBOTA Shota <ta2+github@o2p.jp>
2024-03-23 18:41:58 +0900

    fix(HeroImage): fix vertically stretched image display

--- workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
+++ workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
@@ -1,19 +1,10 @@
 import styled from 'styled-components';
 
-const _Wrapper = styled.div`
-  aspect-ratio: 16 / 9;
-  width: 100%;
-`;
-
 const _Image = styled.img`
-  display: inline-block;
-  width: 100%;
+  height: auto;
+  max-width: 100%;
 `;
 
-export const HeroImage: React.FC = () => {
-  return (
-    <_Wrapper>
-      <_Image alt="Cyber TOON" height="576" src="/assets/hero-image.png" width="1024" />
-    </_Wrapper>
-  );
-};
+export const HeroImage: React.FC = () => (
+  <_Image alt="Cyber TOON" height="576" src="/assets/hero-image.png" width="1024" />
+);
