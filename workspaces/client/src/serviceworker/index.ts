a2cbe5029618323f547cbaa88ff33a730f9292ec
KUBOTA Shota <ta2+github@o2p.jp>
2024-03-23 23:31:31 +0900

    feat(serviceworker): stop imports are p-queue and transformJpegXLToBmp

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,12 +1,4 @@
 /// <reference types="@types/serviceworker" />
-import PQueue from 'p-queue';
-
-import { transformJpegXLToBmp } from './transformJpegXLToBmp';
-
-// ServiceWorker が負荷で落ちないように並列リクエスト数を制限する
-const queue = new PQueue({
-  concurrency: 5,
-});
 
 self.addEventListener('install', (ev: ExtendableEvent) => {
   ev.waitUntil(self.skipWaiting());
@@ -15,21 +7,3 @@ self.addEventListener('install', (ev: ExtendableEvent) => {
 self.addEventListener('activate', (ev: ExtendableEvent) => {
   ev.waitUntil(self.clients.claim());
 });
-
-self.addEventListener('fetch', (ev: FetchEvent) => {
-  ev.respondWith(
-    queue.add(() => onFetch(ev.request), {
-      throwOnTimeout: true,
-    }),
-  );
-});
-
-async function onFetch(request: Request): Promise<Response> {
-  const res = await fetch(request);
-
-  if (res.headers.get('Content-Type') === 'image/jxl') {
-    return transformJpegXLToBmp(res);
-  } else {
-    return res;
-  }
-}
