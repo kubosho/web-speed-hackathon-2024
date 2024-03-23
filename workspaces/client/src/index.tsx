eeacbf4c9e91787f1b766972748a219d4a751ffe
KUBOTA Shota <ta2+github@o2p.jp>
2024-03-24 01:18:18 +0900

    perf(client/index): use dynamic import

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -2,17 +2,16 @@ import { createRoot, hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
 
-import { AdminApp } from '@wsh-2024/admin/src/index';
-import { ClientApp } from '@wsh-2024/app/src/index';
-
 import { registerServiceWorker } from './utils/registerServiceWorker';
 
 const main = async () => {
   await registerServiceWorker();
 
   if (window.location.pathname.startsWith('/admin')) {
+    const { AdminApp } = await import('@wsh-2024/admin/src/index');
     createRoot(document.getElementById('root')!).render(<AdminApp />);
   } else {
+    const { ClientApp } = await import('@wsh-2024/app/src/index');
     hydrateRoot(
       document.getElementById('root')!,
       <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
