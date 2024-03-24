da5cd82a26893f2baed10b277d2d2c522ff3ec6c
KUBOTA Shota <ta2+github@o2p.jp>
2024-03-24 16:10:35 +0900

    perf(server): use hono/compress instead of compressMiddleware

--- workspaces/server/src/routes/index.ts
+++ workspaces/server/src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Hono } from 'hono';
+import { compress } from 'hono/compress'
 import { cors } from 'hono/cors';
 import { HTTPException } from 'hono/http-exception';
 import { secureHeaders } from 'hono/secure-headers';
-
-import { cacheControlMiddleware } from '../middlewares/cacheControlMiddleware';
-import { compressMiddleware } from '../middlewares/compressMiddleware';
 
 import { adminApp } from './admin';
 import { apiApp } from './api';
@@ -24,7 +22,7 @@ app.use(
     origin: (origin) => origin,
   }),
 );
-app.use(compressMiddleware);
+app.use(compress());
 
 app.get('/healthz', (c) => {
   return c.body('live', 200);
